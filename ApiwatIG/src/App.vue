7429b9e79eab80ea8edb8fc1d0c9770089341d3c
apiwattpk <rohmudece@gmail.com>
2020-08-27 15:02:25 +0700

    update

--- ApiwatIG/src/App.vue
+++ ApiwatIG/src/App.vue
@@ -1,23 +1,43 @@
 <template>
-<div id="app">
-    <h1> Rohmud TPK </h1>
-    <router-view />
-</div>
+  <div id="app">
+    <div class="ui inverted segment">
+      <div class="ui inverted secondary pointing menu">
+        <a class="active item">
+          Home
+        </a>
+        <a class="item" :href="contactLink">
+          Contact
+        </a>
+        <a class="item">
+          Friends
+        </a>
+      </div>
+    </div>
+
+    <router-view/>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'App'
+  name: 'App',
+  data: function() {
+    return {
+      contactLink: '/contact'
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 2em;
+  margin-top: 0;
 }
 </style>
---
+++ ApiwatIG/src/components/GridImages.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="ui card">
+        <div class="content">
+            <div class="right floated meta">14h</div>
+            <img class="ui avatar image" src="https://avatars0.githubusercontent.com/u/3336816?s=460&u=a3def04c56afd7220d835005dbf1879a0b497554&v=4"> Elliot
+        </div>
+        <div class="image">
+            <img src="https://avatars0.githubusercontent.com/u/3336816?s=460&u=a3def04c56afd7220d835005dbf1879a0b497554&v=4">
+        </div>
+        <div class="content">
+            <span class="right floated">
+            <i class="heart outline like icon"></i>
+            17 likes
+            </span>
+            <i class="comment icon"></i>
+            3 comments
+        </div>
+        <div class="extra content">
+            <div class="ui large transparent left icon input">
+            <i class="heart outline icon"></i>
+            <input type="text" placeholder="Add Comment...">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GridImages'
+}
+</script>
+
+<style>
+
+</style>
